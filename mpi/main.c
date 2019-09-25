377315b1c5df7db19fbf15df0b7083d67e4736b2
solovek <matheus.schmalfuss@gmail.com>
2019-09-25 16:32:47 -0300

    mpi merge. still need to make the host join the sorted sub arrays

---
+++ mpi/main.c
@@ -0,0 +1,66 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int* rndarr (int);
+void msort  (int*, int);
+void prnarr (int*, int);
+
+int g_comm_sz;
+int grank;
+
+int main (int argc, char** argv)
+{
+  int* v = NULL;
+  int  sz;
+  int  psz;
+  int  i;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &g_comm_sz);
+  MPI_Comm_rank(MPI_COMM_WORLD, &grank);
+
+  if (argc == 2) {
+    sz = atoi(argv[1]);
+    
+    if (!grank) { /* top */
+      srand(time(NULL));
+      v = rndarr(sz);
+    }
+
+    msort(v, sz);
+
+    /*
+    if (!grank)
+      prnarr(v, sz);
+    */
+  } else {
+    printf("usage: %s size\n", argv[0]);
+  }
+    
+  MPI_Finalize();
+  return 0;
+}
+
+int* rndarr (int sz)
+{
+  int* v;
+  int  i;
+
+  v = malloc(sz * sizeof(int));
+
+  for (i = 0; i < sz; i++)
+    v[i] = rand() % 100;
+
+  return v;
+}
+
+void prnarr (int* v, int sz)
+{
+  int i;
+
+  for (i = 0; i < sz; i++)
+    printf("%d ", v[i]);
+  printf("\n");
+}
---
+++ mpi/msort.c
@@ -0,0 +1,105 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+static void split (int*, int, int);
+static void merge (int*, int, int, int);
+
+extern int g_comm_sz;
+extern int grank;
+
+void msort (int* v, int sz)
+{
+  int psz;
+  int i;
+  int* w;
+  
+  psz = sz / (g_comm_sz - 1);
+  w = malloc(psz * sizeof(int));
+  
+  if (!grank) { /* top */
+    for (i = 1; i < g_comm_sz; i++) {
+      MPI_Send(v + psz * (i-1), psz, MPI_INT, i, 0, MPI_COMM_WORLD);
+    }
+    
+    for (i = 1; i < g_comm_sz; i++) {
+      /*receive sorted sub arrays here*/
+      MPI_Recv(w, psz, MPI_INT, i, 0, MPI_COMM_WORLD,
+	                              MPI_STATUS_IGNORE);
+      prnarr(w, psz);
+    }
+  } else { /* bottoms */
+    MPI_Recv(w, psz, MPI_INT, 0, 0, MPI_COMM_WORLD,
+	                            MPI_STATUS_IGNORE);
+
+    split(w, 0, psz);
+    
+    MPI_Send(w, psz, MPI_INT, 0, 0, MPI_COMM_WORLD);
+  }
+}
+
+static void split (int* v, int l, int r)
+{
+  int m;
+  
+  if (l < r) {
+    m = (l + r) / 2;
+    split(v,   l, m);
+    split(v, m+1, r);
+
+    merge(v, l, m, r);
+  }
+}
+
+static void merge (int* v, int l, int m, int r)
+{
+  int i, j, k; 
+  int n1 = m - l + 1; 
+  int n2 = r - m; 
+  
+  /* create temp arrays */
+  int* L = malloc(n1 * sizeof(int));
+  int* R = malloc(n1 * sizeof(int));
+  
+  /* Copy data to temp arrays L[] and R[] */
+  for (i = 0; i < n1; i++) 
+    L[i] = v[l + i]; 
+  for (j = 0; j < n2; j++) 
+    R[j] = v[m + 1+ j]; 
+  
+  /* Merge the temp arrays back into arr[l..r]*/
+  i = 0; // Initial index of first subarray 
+  j = 0; // Initial index of second subarray 
+  k = l; // Initial index of merged subarray 
+
+  while (i < n1 && j < n2) { 
+    if (L[i] <= R[j]) { 
+      v[k] = L[i]; 
+      i++; 
+    } else { 
+      v[k] = R[j]; 
+      j++; 
+    } 
+    k++; 
+  } 
+  
+  /* Copy the remaining elements of L[], if there 
+     are any */
+  while (i < n1) { 
+    v[k] = L[i]; 
+    i++; 
+    k++; 
+  } 
+  
+  /* Copy the remaining elements of R[], if there 
+     are any */
+  while (j < n2) { 
+    v[k] = R[j]; 
+    j++; 
+    k++; 
+  }
+
+  free(L);
+  free(R);
+}
