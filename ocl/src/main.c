264e4ce97ec21d6acbf33e6eed8760ea31deefec
solovek <matheus.schmalfuss@gmail.com>
2019-10-19 23:40:47 -0300

    separated kernel calling code into a function

--- ocl/src/main.c
+++ ocl/src/main.c
@@ -5,19 +5,24 @@
 #include <string.h>
 #include <time.h>
 
-extern void   prnarrf  (float*, int);
-extern float* rndarrf  (int);
-extern char*  readfile (char*);
-extern char** loadpath (char*, int*);
+void   prnarrf  (float*, int);
+float* rndarrf  (int);
+char*  readfile (char*);
+char** loadpath (char*, int*);
 
-extern float(* flookup(char*)) (float);
+float(* flookup(char*)) (float);
 
-int gsz = 5;
-
-float foo (float);
+cl_int clCall3 (char*, float*, float*, float*, int);
 
 const char* const errstr_usage =
   "usage: %s [vmult/integral] [function] [sample] [start] [end]\n";
+
+int gsz = 5;
+
+cl_device_id     gdevice;
+cl_context       gcontext;
+cl_command_queue gcommands;
+cl_program       gprogram;
 
 int main (int argc, char** argv)
 {
@@ -27,15 +32,8 @@ int main (int argc, char** argv)
   float* h_b;
   float* h_c;
   int    length;
-  size_t global;
   char** kernel_src;
   int    nofiles;
-
-  cl_device_id     device_id;
-  cl_context       context;
-  cl_command_queue commands;
-  cl_program       program;
-  cl_kernel        kernel;
 
   cl_mem d_a;
   cl_mem d_b;
@@ -55,26 +53,26 @@ int main (int argc, char** argv)
   
   for (i = 0; i < noplatforms; i++)
     if ((err = clGetDeviceIDs(platform[i], CL_DEVICE_TYPE_DEFAULT,
-			      1, &device_id, NULL)) == CL_SUCCESS)
+			      1, &gdevice, NULL)) == CL_SUCCESS)
       break;
 
   /* add check for device_id here */
 
-  context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+  gcontext = clCreateContext(0, 1, &gdevice, NULL, NULL, &err);
 
-  commands = clCreateCommandQueueWithProperties(context, device_id,
-						      0, &err);
+  gcommands = clCreateCommandQueueWithProperties(gcontext, gdevice,
+						 0,        &err);
 
   kernel_src = loadpath("./cl_kernels/", &nofiles);
 
-  program = clCreateProgramWithSource(context,    nofiles,
-				      kernel_src, NULL,
-				      &err);
-  err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+  gprogram = clCreateProgramWithSource(gcontext,   nofiles,
+				       kernel_src, NULL,
+				       &err);
+  err = clBuildProgram(gprogram, 0, NULL, NULL, NULL, NULL);
 
   if (err == CL_BUILD_PROGRAM_FAILURE) {
     char str[1024];
-    clGetProgramBuildInfo(program,              device_id,
+    clGetProgramBuildInfo(gprogram,             gdevice,
 			  CL_PROGRAM_BUILD_LOG, 1024,
 			  str,                  NULL);
     printf("%s\n", str);
@@ -88,11 +86,17 @@ int main (int argc, char** argv)
     h_c = rndarrf(gsz);
 
     length = gsz * sizeof(*h_a);
-    kernel = clCreateKernel(program, "vmult", &err);
+    
+    clCall3("vmult", h_a, h_b, h_c, length);
+    
+    prnarrf(h_a, gsz);
+    prnarrf(h_b, gsz);
+    prnarrf(h_c, gsz);
   } else if (!strcmp(argv[1], "integral")) { /* exercise 3 */
     float(* f)(float); /* integration target */
     float   x;
     float   step;
+    float   acc;
     
     if (argc < 6) {
       printf(errstr_usage, argv[0]);
@@ -114,44 +118,8 @@ int main (int argc, char** argv)
       h_b[i] = x;
     }
 
-    kernel = clCreateKernel(program, "trap", &err);
-  }
-
-  d_a = clCreateBuffer(context, CL_MEM_READ_ONLY,
-		       length,  NULL, &err);
-  d_b = clCreateBuffer(context, CL_MEM_READ_ONLY,
-		       length,  NULL, &err);
-  d_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-		       length,  NULL, &err);
-  
-  err = clEnqueueWriteBuffer(commands, d_a, CL_TRUE,    0,
-			     length,   h_a,       0, NULL,
-			     NULL);
-  err = clEnqueueWriteBuffer(commands, d_b, CL_TRUE,    0,
-			     length,   h_b,       0, NULL,
-			     NULL);
-
-  err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_a);
-  err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
-  err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
-  err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &gsz);
-
-  global = gsz;
-  err = clEnqueueNDRangeKernel(commands, kernel,  1,
-			       NULL,     &global, NULL,
-			       0,        NULL,    NULL);
-
-  err = clFinish(commands);
-
-  err = clEnqueueReadBuffer(commands, d_c, CL_TRUE, 0,
-			    length,   h_c, 0,       NULL,
-			    NULL);
-  if (!strcmp(argv[1], "vmult")) {
-    prnarrf(h_a, gsz);
-    prnarrf(h_b, gsz);
-    prnarrf(h_c, gsz);
-  } else if (!strcmp(argv[1], "integral")) {
-    float acc;
+    clCall3("trap", h_a, h_b, h_c, length);
+    
     for (i = 0, acc = 0; i < gsz-1; i++) {
       acc += h_c[i];
     }
@@ -163,14 +131,10 @@ int main (int argc, char** argv)
   free(h_b);
   free(h_c);
   
-  clReleaseMemObject(d_a);
-  clReleaseMemObject(d_b);
-  clReleaseMemObject(d_c);
-  clReleaseKernel(kernel);
  argv_err:
-  clReleaseProgram(program);
-  clReleaseCommandQueue(commands);
-  clReleaseContext(context);
+  clReleaseProgram(gprogram);
+  clReleaseCommandQueue(gcommands);
+  clReleaseContext(gcontext);
 
   for (i = 0; i < nofiles; i++)
     free(kernel_src[i]);
@@ -179,7 +143,55 @@ int main (int argc, char** argv)
   return 0;
 }
 
-float foo (float x)
+cl_int clCall3 (char*  name,
+		float* h_a,
+		float* h_b,
+		float* h_c,
+		int    len)
 {
-  return 10;
+  cl_int err;
+  cl_mem d_a;
+  cl_mem d_b;
+  cl_mem d_c;
+  size_t global;
+  cl_kernel kernel;
+  
+  kernel = clCreateKernel(gprogram, name, &err);
+  
+  d_a = clCreateBuffer(gcontext, CL_MEM_READ_ONLY,
+		       len,      NULL, &err);
+  d_b = clCreateBuffer(gcontext, CL_MEM_READ_ONLY,
+		       len,      NULL, &err);
+  d_c = clCreateBuffer(gcontext, CL_MEM_WRITE_ONLY,
+		       len,      NULL, &err);
+  
+  err = clEnqueueWriteBuffer(gcommands, d_a, CL_TRUE,    0,
+			     len,       h_a,       0, NULL,
+			     NULL);
+  err = clEnqueueWriteBuffer(gcommands, d_b, CL_TRUE,    0,
+			     len,       h_b,       0, NULL,
+			     NULL);
+
+  err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_a);
+  err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
+  err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
+  err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &gsz);
+
+  global = gsz;
+  err = clEnqueueNDRangeKernel(gcommands, kernel,  1,
+			       NULL,      &global, NULL,
+			       0,         NULL,    NULL);
+
+  err = clFinish(gcommands);
+
+  err = clEnqueueReadBuffer(gcommands, d_c, CL_TRUE, 0,
+			    len,       h_c, 0,       NULL,
+			    NULL);
+
+  clReleaseMemObject(d_a);
+  clReleaseMemObject(d_b);
+  clReleaseMemObject(d_c);
+  clReleaseKernel(kernel);
+  
+  return 0;
 }
